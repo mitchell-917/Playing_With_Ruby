69414c298b83a31aa42022467c6ef12e5e0bd713
mitchell-917 <mitchell.perry001@gmail.com>
2021-05-15 14:26:18 +0100

    saving progress

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
 class Calculator
 
     calculator_active = true
+
     while calculator_active == true
 
         puts "Would you like to [a]dd, [m]ultiply, [s]ubtract or [d]ivide?"
@@ -19,17 +20,17 @@ class Calculator
 
         def subtract(n1,n2)
             result = n1 - n2
-            puts "Your calculation is: #{n1} - #{n2} = #{result}"            
+            puts "Your calculation is: #{n1} - #{n2} = #{result}"
         end
 
         def multiply(n1,n2)
             result = n1 * n2
-            puts "Your calculation is: #{n1} * #{n2} = #{result}"            
+            puts "Your calculation is: #{n1} * #{n2} = #{result}"
         end
         
         def divide(n1,n2)
             result = n1 / n2
-            puts "Your calculation is: #{n1} / #{n2} = #{result}"            
+            puts "Your calculation is: #{n1} / #{n2} = #{result}"
         end  
 
         if method_response == 'a'
@@ -39,22 +40,21 @@ class Calculator
         elsif method_response == 'm' 
             calculate.multiply(@num1,@num2) 
         elsif method_response == 'd'
-            calculate.divide(@num1,@num2)        
+            calculate.divide(@num1,@num2)
         end
         
         puts "Would you like to make another calcualtion, [y]es or [n]o ?"
         run_again_response = gets.chomp.downcase
-
         if run_again_response == 'y'
             sleep(0.5)
             puts "Rebuilding Calculator..."
             sleep(1)
             puts "Restarting Now!"
-            sleep(1)                                  
+            sleep(1)
         elsif run_again_response == 'n'
             puts "Thank for your using this Calculator, Goodbye!"
             sleep(1)
-            calculator_active = false     
-        end           
+            calculator_active = false
+        end
     end
-end            
+end
