3e56846f49b315f80edb277a36d9b3c8dd53b4cb
mitchell-917 <mitchell.perry001@gmail.com>
2021-05-15 13:30:51 +0100

    Saving progress

--- calculator.rb
+++ calculator.rb
@@ -5,19 +5,29 @@ num1 = gets.chomp.to_i
 num2 = gets.chomp.to_i
 
 
+    if method_response == 'a'
+        
+    elsif method_response == 'm' 
+
+    elsif method_response == 's' 
+        
+    elsif method_response == 'd'
+        
+    end    
+
 
     def add(n1,n2)
-
+        n1 + n2
     end
 
     def subtract(n1,n2)
-
+        n1 - n2
     end
 
     def multiply(n1,n2)
-
+        n1 * n2
     end
     
     def divide(n1,n2)
-        
+        n1 / n2
     end    
