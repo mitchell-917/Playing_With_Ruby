da189d6579bae6f7abd4988ecefa48cf511f58c3
mitchell-917 <mitchell.perry001@gmail.com>
2021-05-15 14:01:32 +0100

    Saving Progress

--- calculator.rb
+++ calculator.rb
@@ -13,19 +13,20 @@ class Calculator
         calculate = Calculator.new
 
         def add(n1,n2)
-            puts n1 + n2
+            result = n1 + n2
+            puts "#{n1} + #{n2} = #{result}"
         end
 
         def subtract(n1,n2)
-            puts n1 - n2
+            result = n1 - n2
         end
 
         def multiply(n1,n2)
-            puts n1 * n2
+            result = n1 * n2
         end
         
         def divide(n1,n2)
-            puts n1 / n2
+            result = n1 / n2
         end  
 
         if method_response == 'a'
@@ -33,7 +34,7 @@ class Calculator
         elsif method_response == 'm' 
             calculate.subtract(@num1,@num2)
         elsif method_response == 's' 
-            calculate.multiply(@num1,@num2)        
+            calculate.multiply(@num1,@num2) 
         elsif method_response == 'd'
             calculate.divide(@num1,@num2)        
         end
@@ -46,7 +47,7 @@ class Calculator
         elsif run_again == 'n'
             calculator_active = false
         else 
-            puts "Please use y for Yes or n for No"
+            puts "Please use 'y' for Yes or 'n' for No"
         end        
     end
 end            
